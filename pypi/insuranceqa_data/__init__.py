607bdba2d09fa9ae811b8d78eff09160a7a459fc
lxm <l11x0m7@bupt.edu.cn>
2018-01-23 23:53:36 +0800

    update insuranceqa_data

--- pypi/insuranceqa_data/__init__.py
+++ pypi/insuranceqa_data/__init__.py
@@ -8,6 +8,7 @@
 # File: /Users/hain/ai/insuranceqa-corpus-zh/pypi/insuranceqa_data/__init__.py
 # Author: Hai Liang Wang
 # Date: 2017-07-28:10:47:28
+# Modified by Xuming Lin in 2018-01-23 23:50
 #
 #===============================================================================
 from __future__ import print_function
@@ -27,26 +28,34 @@ import wget
 
 def load(data_path):
     if not os.path.exists(data_path):
+        data_dir = os.path.split(os.path.split(data_path)[0])[0]
+        if not os.path.exists(data_dir):
+            os.mkdir(data_dir)
+        if not os.path.exists(os.path.join(data_dir, 'pairs')):
+            os.mkdir(os.path.join(data_dir, 'pairs'))
+        if not os.path.exists(os.path.join(data_dir, 'pool')):
+            os.mkdir(os.path.join(data_dir, 'pool'))
+        print('\n Now download data to dir {}'.format(data_dir))
         # download all pair data
         print("\n [insuranceqa_data] downloading data %s ... \n" % "https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.test.json.gz")
-        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.test.json.gz", out = os.path.join(curdir, 'pairs'))
+        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.test.json.gz", out = os.path.join(data_dir, 'pairs'))
         print("\n [insuranceqa_data] downloading data %s ... \n" % "https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.train.json.gz")
-        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.train.json.gz", out = os.path.join(curdir, 'pairs'))
+        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.train.json.gz", out = os.path.join(data_dir, 'pairs'))
         print("\n [insuranceqa_data] downloading data %s ... \n" % "https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.valid.json.gz")
-        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.valid.json.gz", out = os.path.join(curdir, 'pairs'))
+        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.valid.json.gz", out = os.path.join(data_dir, 'pairs'))
         print("\n [insuranceqa_data] downloading data %s ... \n" % "https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.vocab.json.gz")
-        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.vocab.json.gz", out = os.path.join(curdir, 'pairs'))
+        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pairs/iqa.vocab.json.gz", out = os.path.join(data_dir, 'pairs'))
 
         # download all pool data
         print("\n [insuranceqa_data] downloading data %s ... \n" % "https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/answers.json.gz")
-        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/answers.json.gz", out = os.path.join(curdir, 'pool'))
+        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/answers.json.gz", out = os.path.join(data_dir, 'pool'))
         print("\n [insuranceqa_data] downloading data %s ... \n" % "https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/test.json.gz")
-        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/test.json.gz", out = os.path.join(curdir, 'pairs'))
+        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/test.json.gz", out = os.path.join(data_dir, 'pool'))
         print("\n [insuranceqa_data] downloading data %s ... \n" % "https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/train.json.gz")
-        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/train.json.gz", out = os.path.join(curdir, 'pairs'))
+        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/train.json.gz", out = os.path.join(data_dir, 'pool'))
         print("\n [insuranceqa_data] downloading data %s ... \n" % "https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/valid.json.gz")
-        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/valid.json.gz", out = os.path.join(curdir, 'pairs'))
-        
+        wget.download("https://github.com/Samurais/insuranceqa-corpus-zh/raw/release/corpus/pool/valid.json.gz", out = os.path.join(data_dir, 'pool'))
+
     with gzip.open(data_path, 'rb') as f:
         data = json.loads(f.read())
         return data
@@ -54,21 +63,33 @@ def load(data_path):
 '''
 pool data are translated Chinese data with Google API from original English data
 '''
-POOL_TEST_DATA = os.path.join(curdir, 'pool', 'test.json.gz')
-POOL_TRAIN_DATA = os.path.join(curdir, 'pool', 'train.json.gz')
-POOL_VALID_DATA = os.path.join(curdir, 'pool', 'valid.json.gz')
-POOL_ANS_DATA = os.path.join(curdir, 'pool', 'answers.json.gz')
 
-def load_pool_test():
+def load_pool_test(data_path=None):
+    if data_path is None:
+        POOL_TEST_DATA = os.path.join(curdir, 'pool', 'test.json.gz')
+    else:
+        POOL_TEST_DATA = os.path.join(data_path, 'pool', 'test.json.gz')
     return load(POOL_TEST_DATA)
 
-def load_pool_valid():
+def load_pool_valid(data_path=None):
+    if data_path is None:
+        POOL_VALID_DATA = os.path.join(curdir, 'pool', 'valid.json.gz')
+    else:
+        POOL_VALID_DATA = os.path.join(data_path, 'pool', 'valid.json.gz')
     return load(POOL_VALID_DATA)
 
-def load_pool_train():
-    return load(POOL_TRAIN_DATA)
+def load_pool_train(data_path=None):
+    if data_path is None:
+        POOL_TRAIN_DATA = os.path.join(curdir, 'pool', 'train.json.gz')
+    else:
+        POOL_TRAIN_DATA = os.path.join(data_path, 'pool', 'train.json.gz')
+        return load(POOL_TRAIN_DATA)
 
-def load_pool_answers():
+def load_pool_answers(data_path=None):
+    if data_path is None:
+        POOL_ANS_DATA = os.path.join(curdir, 'pool', 'answers.json.gz')
+    else:
+        POOL_ANS_DATA = os.path.join(data_path, 'pool', 'answers.json.gz')
     return load(POOL_ANS_DATA)
 
 def __test_qa():
@@ -84,24 +105,36 @@ def __test_answers():
 '''
 pair data are segmented and labeled after pool data
 '''
-PAIR_TEST_DATA = os.path.join(curdir, 'pairs','iqa.test.json.gz')
-PAIR_VALID_DATA = os.path.join(curdir, 'pairs','iqa.valid.json.gz')
-PAIR_TRAIN_DATA = os.path.join(curdir, 'pairs','iqa.train.json.gz')
-PAIR_VOCAB_DATA = os.path.join(curdir, 'pairs', 'iqa.vocab.json.gz')
 
-def load_pairs_vocab():
+def load_pairs_vocab(data_path=None):
     '''
     Load vocabulary data
     '''
+    if data_path is None:
+        PAIR_VOCAB_DATA = os.path.join(curdir, 'pairs', 'iqa.vocab.json.gz')
+    else:
+        PAIR_VOCAB_DATA = os.path.join(data_path, 'pairs', 'iqa.vocab.json.gz')
     return load(PAIR_VOCAB_DATA)
 
-def load_pairs_test():
+def load_pairs_test(data_path=None):
+    if data_path is None:
+        PAIR_TEST_DATA = os.path.join(curdir, 'pairs','iqa.test.json.gz')
+    else:
+        PAIR_TEST_DATA = os.path.join(data_path, 'pairs','iqa.test.json.gz')
     return load(PAIR_TEST_DATA)
 
-def load_pairs_valid():
+def load_pairs_valid(data_path=None):
+    if data_path is None:
+        PAIR_VALID_DATA = os.path.join(curdir, 'pairs','iqa.valid.json.gz')
+    else:
+        PAIR_VALID_DATA = os.path.join(data_path, 'pairs','iqa.valid.json.gz')
     return load(PAIR_VALID_DATA)
 
-def load_pairs_train():
+def load_pairs_train(data_path=None):
+    if data_path is None:
+        PAIR_TRAIN_DATA = os.path.join(curdir, 'pairs','iqa.train.json.gz')
+    else:
+        PAIR_TRAIN_DATA = os.path.join(data_path, 'pairs','iqa.train.json.gz')
     return load(PAIR_TRAIN_DATA)
 
 def __test_pair_test():
